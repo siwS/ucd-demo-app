525b12883e99f1bc2231107fdc668e31634d653b
sofia <sofia.tzima@intercom.io>
2018-05-23 06:56:31 +0100

    remove unnecessary serializer

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,5 @@ module RubyGettingStarted
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    Rails.logger.info("Application.rb initialization")
-
-    ConsoleAccessCheck.configure do |config|
-      Rails.logger.info("Initializing Console Access check...")
-      config.raise_error = true
-      config.sensitive_tables = ["users"]
-    end
   end
 end
