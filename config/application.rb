63ec5f93cde7f35c8c2f37602b05028cc224d4fd
sofia <sofia.tzima@intercom.io>
2018-05-23 06:39:15 +0100

    Add configuration

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module RubyGettingStarted
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    ConsoleAccessCheck.configure do |config|
+      config.sensitive_tables = ["users"]
+      config.raise_error = true
+    end
   end
 end
